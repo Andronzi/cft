5a39abfbae3bb6ac30da712ef8d2bc58b9d81af4
Andrey Radionov <andrey.radioniv.2003@gmail.com>
2022-09-28 20:27:42 +0700

    cleaning trash coding moments

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -1,18 +1,27 @@
+import webpack from "webpack-stream";
+import env from "dotenv";
+env.config();
+
 export const js = () => {
-  return (
-    app.gulp
-      .src(app.path.src.js)
-      .pipe(
-        app.plugins.plumber(
-          app.plugins.notify.onError({
-            title: "JS",
-            subtitle: "Fail JS COMPILATION",
-            message: "Error: <%= error.message %>",
-          })
-        )
+  return app.gulp
+    .src(app.path.src.js)
+    .pipe(
+      app.plugins.plumber(
+        app.plugins.notify.onError({
+          title: "JS",
+          subtitle: "Fail JS COMPILATION",
+          message: "Error: <%= error.message %>",
+        })
       )
-      // .pipe(uglify())
-      .pipe(app.gulp.dest(app.path.build.js))
-      .pipe(app.plugins.browserSync.stream())
-  );
+    )
+    .pipe(
+      webpack({
+        mode: process.env.mode,
+        output: {
+          filename: "app.min.js",
+        },
+      })
+    )
+    .pipe(app.gulp.dest(app.path.build.js))
+    .pipe(app.plugins.browserSync.stream());
 };
