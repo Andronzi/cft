4217b4ae3910b8f2c6b58ad7cc309219498ae18d
Andrey Radionov <andrey.radioniv.2003@gmail.com>
2022-09-28 20:32:18 +0700

    project build

--- src/js/helpers/string.helper.js
+++
@@ -1,5 +0,0 @@
-const getPartOfString = (string, prevIndex, currentIndex) => {
-  return string.substr(prevIndex, currentIndex);
-};
-
-export default getPartOfString;
--- src/js/searchObject.js
+++
@@ -1,7 +0,0 @@
-export default {
-  "Search Algorithms": 1,
-  "Graph Algorithms": 3,
-  "Sort Algorithms": 5,
-  Structures: 7,
-  "Big O notation": 9,
-};
